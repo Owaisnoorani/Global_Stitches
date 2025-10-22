69ebdb4611dd057c597dd04cc15374e5a5e167c9
Owaisnoorani <owaisabbasi331@gmail.com>
2025-10-22 17:45:16 +0500

    change contact info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,9 +28,9 @@ const Footer = () => {
         {/* Contact */}
         <div className="footer-contact">
           <h3>Contact Us</h3>
-          <p>📍 Karachi, Pakistan</p>
+          <p>📍 Los Angeles, USA</p>
           <p>📧 support@globalstitches.co</p>
-          <p>📞 +92 300 1234567</p>
+          <p>📞 +966 55 638 1633</p>
         </div>
 
         {/* Socials */}
